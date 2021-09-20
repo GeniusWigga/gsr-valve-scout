91fde4dca9ca2ea0f6a10c76d63d4fecce77d106
konstantinaxt <axtkonstantin@hotmail.de>
2021-09-20 15:16:59 +0200

    fix styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ValveScout from "./container/ValveScout.jsx";
 
+import "./scss/index.scss";
+
 const valveScoutElem = document.getElementById("valve-scout");
 const locale = valveScoutElem.getAttribute("data-locale");
 
