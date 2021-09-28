48eeb026c70e0106d1b4c4e58816d8491357d826
konstantinaxt <axtkonstantin@hotmail.de>
2021-09-28 17:20:01 +0200

    cleanup

--- src/reflux/actions.js
+++
@@ -1,9 +0,0 @@
-import Reflux from "reflux";
-
-export const ToolTipActions = Reflux.createActions(["showToolTip"]);
-
-export const HelpTextActions = Reflux.createActions(["showHelpText"]);
-
-export const QuestionAnimationWrapperActions = Reflux.createActions(["scrolledToResults", "scrolledInitial"]);
-
-export const FormActions = Reflux.createActions(["showForm"]);
